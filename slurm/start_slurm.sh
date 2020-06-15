23885120a95875f4b49370d36ea4670104edff41
Andre Maximo <andmax@gmail.com>
2020-06-15 13:57:44 -0300

    Update start_slurm.sh
    Testing SLURM's GPU routing using no_consume

--- slurm/start_slurm.sh
+++ slurm/start_slurm.sh
@@ -9,7 +9,7 @@ sed "s/SlurmctldHost=/SlurmctldHost=$(head -n 1 /etc/JARVICE/nodes)/g" /usr/loca
 
 #cat /etc/JARVICE/nodes | while read node; do echo -e "NodeName=$node RealMemory=$(expr $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1024) Sockets=$(lscpu | grep Socket\(s\) | awk '{print $2}') CoresPerSocket=$(lscpu | grep Core\(s\) | awk '{print $4}') ThreadsPerCore=$(lscpu | grep Thread\(s\) | awk '{print $4}') Gres=gpu:tesla_k80:no_consume:1 State=UNKNOWN\n"; done >> /usr/local/etc/slurm.conf
 
-cat /etc/JARVICE/nodes | while read node; do echo -e "NodeName=$node RealMemory=$(expr $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1024) Procs=$(nproc) Gres=gpu:$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | head -n 1 | tr '[:upper:]' '[:lower:]' | tr ' ' '_'):no_consume:$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | wc -l),gpu_mem:$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits | head -n 1) State=UNKNOWN\n"; done >> /usr/local/etc/slurm.conf
+cat /etc/JARVICE/nodes | while read node; do echo -e "NodeName=$node RealMemory=$(expr $(grep MemTotal /proc/meminfo | awk '{print $2}') / 1024) Procs=$(nproc) Gres=gpu:$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | head -n 1 | tr '[:upper:]' '[:lower:]' | tr ' ' '_'):$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | wc -l),gpu_mem:$(nvidia-smi --query-gpu=memory.total --format=csv,noheader,nounits | head -n 1) State=UNKNOWN\n"; done >> /usr/local/etc/slurm.conf
 
 echo "PartitionName=all Nodes=$(cat /etc/JARVICE/nodes | tr '\n' ',' | sed s/.$// -) Default=YES MaxTime=INFINITE State=UP" >> /usr/local/etc/slurm.conf
 

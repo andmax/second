8f16438dc1e955365d56a2701131a6ee4eda03df
Andre Maximo <andmax@gmail.com>
2021-02-09 16:28:00 -0300

    Delete slurm directory

--- slurm/stop_slurm.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# @file stop_slurm.sh
-# @brief Script to stop slurm
-# @author Andre Maximo
-# @date May, 2020
-# @copyright The MIT License
-
-ps aux | grep slurm | awk '{print $2}' | xargs sudo kill -9
